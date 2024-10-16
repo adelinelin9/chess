dd922be5f1ac4fd3b327ac319940d9737f143c15
adelinelin9 <134444966+adelinelin9@users.noreply.github.com>
2024-10-16 15:35:15 -0600

    attempted to implement main.java

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -1,12 +1,21 @@
 import chess.*;
+import dataaccess.DatabaseInitializer;
+import dataaccess.DataAccessException;
 import server.Server;
 
 public class Main {
     public static void main(String[] args) {
-        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Server: " + piece);
+        try {
+            DatabaseInitializer.initializeDatabase();
 
-        new Server().run(8080);
+            var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
+            System.out.println("♕ 240 Chess Server: " + piece);
+        } catch (DataAccessException e) {
+            System.err.println("Error initializing the database: " + e.getMessage());
+            e.printStackTrace();
+        }
+
+//        new Server().run(8080);
     }
 }
 
