1ec447e18e968a6e0743f3ea2d8b58e64b3a0a11
adelinelin9 <134444966+adelinelin9@users.noreply.github.com>
2024-10-17 13:58:39 -0600

    tried implementing Server.java

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -5,14 +5,48 @@ import spark.*;
 public class Server {
 
     public int run(int desiredPort) {
+        try {
+            DatabaseInitilizer.initializeDatabase();
+        } catch (DataAccessException e) {
+            e.printStackTrace();
+        }
+
         Spark.port(desiredPort);
 
         Spark.staticFiles.location("web");
 
-        // Register your endpoints and handle exceptions here.
+        UserDAO userDAO = new UserDAOMySQL();
+        GameDAO gameDAO = new GameDAOMySQL();
+        AuthDAO authDAO = new AuthDAOMySQL();
 
-        //This line initializes the server and can be removed once you have a functioning endpoint 
-        Spark.init();
+        UserService userService = new UserService(userDAO, authDAO);
+        GameService gameService = new GameService(gameDAO, authDAO, userDAO);
+
+        UserHandler userHandler = new UserHandler(userService);
+        GameHandler gameHandler = new GameHandler(gameService);
+        WebSocketHandler webSocketHandler = new WebSocketHandler(gameService);
+
+        Spark.webSocket("/ws", webSocketHandler);
+
+        Spark.post("/user", userHandler.register);
+        Spark.post("/session", userHandler.login);
+        Spark.delete("/session", userHandler.logout);
+        Spark.get("/game", gameHandler.listGames);
+        Spark.post("/game", gameHandler.createGame);
+        Spark.put("/game", gameHandler.joinGame);
+
+        Spark.delete("/db", (req, res) -> {
+            try {
+                userDAO.clear();
+                gameDAO.clear();
+                authDAO.clear();
+                res.status(200);
+                return "{}";
+            } catch (Exception e) {
+                res.status(400);
+                return "{\"message\" : \"Error: " + e.getMessage()+ "\"}";
+            }
+        });
 
         Spark.awaitInitialization();
         return Spark.port();
@@ -21,5 +55,9 @@ public class Server {
     public void stop() {
         Spark.stop();
         Spark.awaitStop();
+    }
+
+    public static void main(String[] args) {
+        new Server().run(8080);
     }
 }
