2f419fdc72e50934215c5923c98c259287c06f90
adelinelin9 <134444966+adelinelin9@users.noreply.github.com>
2024-11-12 16:55:50 -0700

    updated Repl to fix message

--- client/src/main/java/client/Repl.java
+++ client/src/main/java/client/Repl.java
@@ -1,3 +1,46 @@
+//package client;
+//
+//import ui.ChessClient;
+//
+//import java.util.Scanner;
+//
+//import static ui.EscapeSequences.*;
+//
+//public class Repl {
+//    private final ChessClient client;
+//
+//    public Repl(String serverURL) {
+//        this.client = new ChessClient(serverURL);
+//    }
+//
+//    public void run() {
+//        System.out.println(YELLOW + "♕ " +
+//                WHITE + BOLD + "Welcome to 240 Chess. Type Help to get started. " +
+//                RESET_BOLD_FAINT + YELLOW + "♕\n");
+//        System.out.println(client.help());
+//
+//        Scanner scanner = new Scanner(System.in);
+//        var result = "";
+//        while (!result.equals("quit")) {
+//            printPrompt();
+//            String line = scanner.nextLine();
+//
+//            try {
+//                result = client.eval(line);
+//                System.out.print(BLUE + result + "\n\n");
+//            } catch (Throwable e) {
+//                var msg = e.toString();
+//                System.out.print(msg + "\n");
+//            }
+//        }
+//        System.out.println();
+//    }
+//
+//    private void printPrompt() {
+//        System.out.print(WHITE + BOLD + "[" + client.getState() + "] " + RESET_BOLD_FAINT + ">>> " + GREEN + FAINT);
+//    }
+//}
+
 package client;
 
 import ui.ChessClient;
@@ -29,7 +72,13 @@ public class Repl {
                 result = client.eval(line);
                 System.out.print(BLUE + result + "\n\n");
             } catch (Throwable e) {
-                var msg = e.toString();
+                var msg = e.getMessage();
+
+                // Check for specific phrases and replace them
+                if (msg != null && msg.contains("Cannot invoke")) {
+                    msg = "Cannot do action because " + msg.substring(msg.indexOf("because"));
+                }
+
                 System.out.print(msg + "\n");
             }
         }
