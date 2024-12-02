ecb341ff84228c790ed3208b6ee3f253fc06c82c
adelinelin9 <134444966+adelinelin9@users.noreply.github.com>
2024-12-02 16:33:08 -0700

    worked on UI

--- client/src/main/java/client/ServerFacade.java
+++ client/src/main/java/client/ServerFacade.java
@@ -1,200 +1,55 @@
 package client;
 
-import com.google.gson.Gson;
-import records.AuthData;
-import records.ListGameData;
-import server.request.game.CreateGameRequest;
-import server.request.game.JoinGameRequest;
+import chess.ChessGame;
 import server.request.user.LoginRequest;
 import server.request.user.RegisterRequest;
 
-import java.io.*;
-import java.net.*;
 import java.util.Collection;
 import java.util.Map;
 
 
 public class ServerFacade {
-    private final String serverURL;
-    private static String authToken;
+    private final HttpCommunicator http;
 
     public ServerFacade(String serverURL) {
-        this.serverURL = serverURL;
-        authToken = null;
+        this.http = new HttpCommunicator(serverURL);
     }
 
     public String register(RegisterRequest request) throws ResponseException {
-        try {
-            var resp = this.makeRequest("POST", "/user", request, AuthData.class);
-            authToken = resp.authToken();
-        } catch (ResponseException e) {
-            if (e.statusCode() == 403) {
-                throw new ResponseException(403, "Username already taken");
-            }
-            else {
-                throw new ResponseException(e.statusCode(), e.getMessage());
-            }
-        }
-        return authToken;
+        return http.register(request);
     }
 
     public String login(LoginRequest request) throws ResponseException {
-        try {
-            var resp = this.makeRequest("POST", "/session", request, AuthData.class);
-            authToken = resp.authToken();
-        } catch (ResponseException e) {
-            if (e.statusCode() == 401) {
-                throw new ResponseException(401, "Username or password incorrect");
-            }
-            else {
-                throw new ResponseException(e.statusCode(), e.getMessage());
-            }
-        }
-
-        return authToken;
+        return http.login(request);
     }
 
     public Boolean logout() throws ResponseException {
-        try {
-            this.makeRequest("DELETE", "/session", null, null);
-        } catch (ResponseException e) {
-            if (e.statusCode() == 401) {
-                throw new ResponseException(401, "You are not logged in");
-            }
-            else {
-                throw new ResponseException(e.statusCode(), e.getMessage());
-            }
-        }
-        return true;
+        return http.logout();
     }
 
     public Collection<Map<String, Object>> listGames() throws ResponseException {
-        try {
-            var resp = this.makeRequest("GET", "/game", null, ListGameData.class);
-            return resp.games();
-        } catch (ResponseException e) {
-            if (e.statusCode() == 401) {
-                throw new ResponseException(401, "You are not logged in");
-            }
-            else {
-                throw new ResponseException(e.statusCode(), e.getMessage());
-            }
-        }
+        return http.listGames();
     }
 
     public Boolean createGame(String gameName) throws ResponseException {
-        try {
-            CreateGameRequest request = new CreateGameRequest(authToken, gameName);
-            this.makeRequest("POST", "/game", request, null);
-        } catch (ResponseException e) {
-            if (e.statusCode() == 401) {
-                throw new ResponseException(401, "You are not logged in");
-            }
-            else {
-                throw new ResponseException(e.statusCode(), e.getMessage());
-            }
-        }
-        return true;
+        return http.createGame(gameName);
     }
 
-    public Boolean joinGame(int gameID, String color) throws ResponseException {
-        try {
-            JoinGameRequest request = new JoinGameRequest(authToken, color, gameID);
-            this.makeRequest("PUT", "/game", request, null);
-            return true;
-        } catch (ResponseException e) {
-            if (e.statusCode() == 401) {
-                throw new ResponseException(401, "You are not logged in");
-            }
-            else if (e.statusCode() == 403) {
-                throw new ResponseException(403, "Color already taken, or game is full");
-            }
-            else if (e.statusCode() == 400) {
-                throw new ResponseException(400, "Game not found, check id");
-            }
-            else {
-                throw new ResponseException(e.statusCode(), e.getMessage());
-            }
-        }
+    public ChessGame joinGame(int gameID, String color) throws ResponseException {
+        return http.joinGame(gameID, color);
     }
 
-    public boolean observeGame(int gameID) throws ResponseException {
-        try {
-            JoinGameRequest request = new JoinGameRequest(authToken, "OBSERVE", gameID);
-            this.makeRequest("PUT", "/game", request, null);
-            return true;
-        } catch (ResponseException e) {
-            if (e.statusCode() == 401) {
-                throw new ResponseException(401, "You are not logged in");
-            }
-            else if (e.statusCode() == 400) {
-                throw new ResponseException(400, "Game not found, check id");
-            }
-            else {
-                throw new ResponseException(e.statusCode(), e.getMessage());
-            }
-        }
+    public void leaveGame(int gameID, String color) throws ResponseException {
+        http.leaveGame(gameID, color);
     }
 
-    private <T> T makeRequest(String method, String path, Object request, Class<T> responseClass) throws ResponseException {
-        try {
-            URL url = (new URI(serverURL + path)).toURL();
-            HttpURLConnection http = (HttpURLConnection) url.openConnection();
-            http.setRequestMethod(method);
-            http.setDoOutput(true);
-
-            writeBody(request, http);
-            http.connect();
-            throwIfNotSuccessful(http);
-            return readBody(http, responseClass);
-        } catch (ResponseException e) {
-            throw new ResponseException(e.statusCode(), e.getMessage());
-        } catch (IOException | URISyntaxException ex) {
-            throw new ResponseException(500, ex.getMessage());
-        }
+    public ChessGame observeGame(int gameID) throws ResponseException {
+        return http.observeGame(gameID);
     }
 
-    private static void writeBody(Object request, HttpURLConnection http) throws IOException {
-        if (authToken != null) {
-            http.addRequestProperty("authorization", authToken);
-        } if (request != null) {
-            http.addRequestProperty("Content-Type", "application/json");
-            String reqData = new Gson().toJson(request);
-            try (OutputStream reqBody = http.getOutputStream()) {
-                reqBody.write(reqData.getBytes());
-            }
-        }
+    public void clear() throws ResponseException {
+        http.clear();
     }
 
-    private void throwIfNotSuccessful(HttpURLConnection http) throws ResponseException, IOException {
-        var status = http.getResponseCode();
-        if (!isSuccessful(status)) {
-            if (status == 403) {
-                throw new ResponseException(403, "Already taken");
-            }
-            else if (status == 401) {
-                throw new ResponseException(401, "Unauthorized");
-            }
-            else if (status == 400) {
-                throw new ResponseException(400, "Bad request");
-            }
-        }
-    }
 
-    private <T> T readBody(HttpURLConnection http, Class<T> responseClass) throws IOException {
-        T response = null;
-        if (http.getContentLength() < 0) {
-            try (InputStream resBody = http.getInputStream()) {
-                InputStreamReader reader = new InputStreamReader(resBody);
-                if (responseClass != null) {
-                    response = new Gson().fromJson(reader, responseClass);
-                }
-            }
-        }
-        return response;
-    }
-
-    private static boolean isSuccessful(int status) {
-        return status / 100 == 2;
-    }
 }
---
+++ client/src/main/java/ui/Repl.java
@@ -0,0 +1,42 @@
+package ui;
+
+import client.ChessClient;
+
+import java.util.Scanner;
+
+import static ui.EscapeSequences.*;
+
+public class Repl {
+    private final ChessClient client;
+
+    public Repl(String serverURL) {
+        this.client = new ChessClient(serverURL);
+    }
+
+    public void run() {
+        System.out.println(YELLOW + "♕ " +
+                WHITE + BOLD + "Welcome to 240 Chess. Type Help to get started. " +
+                RESET_BOLD_FAINT + YELLOW + "♕\n");
+        System.out.println(client.help());
+
+        Scanner scanner = new Scanner(System.in);
+        var result = "";
+        while (!result.equals("quit")) {
+            printPrompt();
+            String line = scanner.nextLine();
+
+            try {
+                result = client.eval(line);
+                System.out.print(BLUE + result + "\n\n");
+            } catch (Throwable e) {
+                var msg = e.toString();
+                System.out.print(msg + "\n");
+            }
+        }
+        System.out.println();
+    }
+
+    private void printPrompt() {
+        System.out.print(WHITE + BOLD + "[" + client.getState() + "] " + RESET_BOLD_FAINT + ">>> " + GREEN + FAINT);
+    }
+}
