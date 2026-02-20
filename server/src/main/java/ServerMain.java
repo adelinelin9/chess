2b91ae2a542264a6a47301a87db33169819b0397
adelinelin9 <134444966+adelinelin9@users.noreply.github.com>
2026-02-20 14:26:20 -0700

    manually moved files to start phase 3

--- server/src/main/java/ServerMain.java
+++ server/src/main/java/ServerMain.java
@@ -1,10 +1,12 @@
-package server;
-
 import chess.*;
+import server.Server;
 
 public class ServerMain {
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Server: " + piece);
+
+        Server server = new Server();
+        server.run(8080);
     }
 }
