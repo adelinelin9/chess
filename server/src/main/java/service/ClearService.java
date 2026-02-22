4e88aecdf692ab30f9862664146e51916dffdd82
adelinelin9 <134444966+adelinelin9@users.noreply.github.com>
2026-02-21 21:57:13 -0700

    worked on ClearService

--- server/src/main/java/service/ClearService.java
+++ server/src/main/java/service/ClearService.java
@@ -1,5 +1,23 @@
 package service;
 
+import dataaccess.MemoryAuthDAO;
+import dataaccess.MemoryGameDAO;
+import dataaccess.MemoryUserDAO;
 
 public class ClearService {
+    private MemoryUserDAO userDAO;
+    private MemoryAuthDAO authDAO;
+    private MemoryGameDAO gameDAO;
+
+    public ClearService(MemoryUserDAO userDAO, MemoryAuthDAO authDAO, MemoryGameDAO gameDAO) {
+        this.userDAO = userDAO;
+        this.authDAO = authDAO;
+        this.gameDAO = gameDAO;
+    }
+
+    public void clear() {
+        userDAO.clear();
+        authDAO.clear();
+        gameDAO.clear();
+    }
 }
