926e79c83118c0c1b3711572e46635883cc32865
adelinelin9 <134444966+adelinelin9@users.noreply.github.com>
2026-03-03 14:12:55 -0700

    implemented GameDAO

---
+++ server/src/main/java/dataaccess/GameDAO.java
@@ -0,0 +1,12 @@
+package dataaccess;
+
+import model.GameData;
+import java.util.List;
+
+public interface GameDAO {
+    int createGame(String gameName) throws DataAccessException;
+    GameData getGame(int gameID) throws DataAccessException;
+    List<GameData> listGames() throws DataAccessException;
+    void updateGame(GameData game) throws DataAccessException;
+    void clear() throws DataAccessException;
+}
