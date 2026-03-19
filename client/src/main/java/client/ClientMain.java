5b411ad9780008ad0fce226ddf8ec7fdbf31727f
adelinelin9 <134444966+adelinelin9@users.noreply.github.com>
2026-03-19 13:08:29 -0600

    updated clientmain

--- client/src/main/java/client/ClientMain.java
+++ client/src/main/java/client/ClientMain.java
@@ -1,10 +1,12 @@
 package client;
 
-import chess.*;
-
 public class ClientMain {
     public static void main(String[] args) {
-        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Client: " + piece);
+        int port = 8080;
+        if (args.length == 1) {
+            port = Integer.parseInt(args[0]);
+        }
+        System.out.println("♕ 240 Chess Client");
+        new ChessClient(port).run();
     }
 }
